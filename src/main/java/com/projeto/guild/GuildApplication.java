8c372dad409d1ee52de33bc6b4419e5d20df66fc
Alexandre Felli Chimello <aleirado@outlook.com>
2026-07-27 14:29:33 -0300

    Project created

---
+++ src/main/java/com/projeto/guild/GuildApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.guild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildApplication.class, args);
+	}
+
+}
